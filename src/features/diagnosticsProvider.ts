7a644541a1e611eb429b8762f1a4776142ae60ec
MisaJovanovic <misaj@nordeus.com>
2018-04-13 20:47:59 +0200

    name

--- src/features/diagnosticsProvider.ts
+++ src/features/diagnosticsProvider.ts
@@ -131,7 +131,7 @@ class DiagnosticsProvider extends AbstractSupport {
         let d4 = vscode.workspace.onDidOpenTextDocument(event => this._onDocumentAddOrChange(event), this);
         let d3 = vscode.workspace.onDidChangeTextDocument(event => this._onDocumentAddOrChange(event.document), this);
         let d5 = vscode.workspace.onDidCloseTextDocument(this._onDocumentRemove, this);
-        let d6 = vscode.window.onDidChangeActiveTextEditor(event => this._onActiveTextEditorChanged(event), this);
+        let d6 = vscode.window.onDidChangeActiveTextEditor(event => this._onDidChangeActiveTextEditor(event), this);
         let d7 = vscode.window.onDidChangeWindowState(event => this._OnDidChangeWindowState(event), this);
         this._disposable = vscode.Disposable.from(this._diagnostics, d1, d2, d3, d4, d5, d6, d7);
 
@@ -158,11 +158,11 @@ class DiagnosticsProvider extends AbstractSupport {
 
     private _OnDidChangeWindowState(windowState: vscode.WindowState): void {
         if (windowState.focused === true) {
-            this._onActiveTextEditorChanged(vscode.window.activeTextEditor);
+            this._onDidChangeActiveTextEditor(vscode.window.activeTextEditor);
         }
     }
 
-    private _onActiveTextEditorChanged(textEditor: vscode.TextEditor): void {
+    private _onDidChangeActiveTextEditor(textEditor: vscode.TextEditor): void {
         // active text editor can be undefined.
         if (textEditor != undefined) { 
             this._onDocumentAddOrChange(textEditor.document);
